64f3b8b2d4565eb26b977644f253163a16cfedb2
sudarshan <sudarshan@dispatchtrack.com>
2017-01-04 00:35:05 +0530

    unsaved data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  paginates_per 5
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
