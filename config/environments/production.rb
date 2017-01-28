90dbf1c9cc4f522e10e8517f6ca2e2ac9eb8e5d8
sudarshan <sudarshan@dispatchtrack.com>
2017-01-28 18:00:32 +0530

    mailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,17 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'sudarshantest1@gmail.com',
+      :password             => 'sandeeppooja',
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+  }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
