f92b2a155a5b73cd5612dc827254d000c917098f
sudarshan <sudarshan@dispatchtrack.com>
2017-02-03 00:48:44 +0530

    Added the Tag modal to create a tag--- without the mailers selection

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,25 @@
 class TagsController < ApplicationController
 	def new
-	
+	@tag=Tag.new
 		
 	end
+
+	def create
+	@tag=Tag.new(post_params)
+
+		if @tag.save
+			flash[:success]="Successfully tag is saved"
+           redirect_to mailerslist_path
+		else
+          flash[:error]=@tag.errors.full_messages.join(',')  
+   #         flash[:error]="Post Not Saved"
+			# redirect_to new_post_path
+	      render :new
+		end
+	end
+
+	private 
+		def post_params
+		params.require(:tag).permit(:name)
+		end
 end
