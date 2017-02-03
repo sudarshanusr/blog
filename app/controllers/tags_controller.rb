19723046a1c1b26800d927782d0d0d75d7aa00d9
sudarshan <sudarshan@dispatchtrack.com>
2017-02-03 23:06:38 +0530

    Tags creation without the class variable @@ids

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,33 +1,19 @@
 class TagsController < ApplicationController
 	def new
-		
-		@@ids=params[:ids]
-
-		@hh=params[:ids]
-
-	@tag=Tag.new
-	
-
-		
+	 @tag=Tag.new
 	end
 
 	def create
-		binding.pry
-	@tag=Tag.new(post_params)
-
-	#@tag.mailer_ids=@@ids
-
-	@tag.mailer_ids=params[:tag][:ids]
-
-	
-
+		@tag=Tag.new(post_params)	
+	    email_ids = params[:tag][:ids].last
+	    email_ids = email_ids.gsub!(/\s/,',')  
+	    email_ids=email_ids.split(',')
+		@tag.mailer_ids=email_ids
 		if @tag.save
 			flash[:success]="Successfully tag is saved"
-           redirect_to mailerslist_path
+	       redirect_to mailerslist_path
 		else
-          flash[:error]=@tag.errors.full_messages.join(',')  
-   #         flash[:error]="Post Not Saved"
-			# redirect_to new_post_path
+	      flash[:error]=@tag.errors.full_messages.join(',')  
 	      render :new
 		end
 	end
