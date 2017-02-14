bf77e187181ca8f5fd21cc3248f24b30795f88b5
sudarshan <sudarshan@dispatchtrack.com>
2017-02-14 15:36:04 +0530

    Added the enable /disable functionality for buttons in Mailerslist & tagslist

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,25 +1,18 @@
-
+ 
 var ids = []
 function check(i){
-    console.log(i)
-    if(document.getElementById('check_'+i).checked) {
-    ids.push(""+i);
-	} 
-	else {
-	ids.pop(""+i);
-    }
-    if(ids.length>0){
-     $("#submit").prop('disabled', false);
-    }else{
-     $("#submit").prop('disabled', true);
-    }
-   console.log(ids);
-}
-$(document).ready(function(){
-  $("#submit").prop('disabled', true);
- console.log("loaded" );
 
-});
+    console.log(i);
+    if(document.getElementById('check_'+i).checked) {
+       ids.push(""+i);
+	  }else {
+	    ids.pop(""+i);
+    }
+    
+    
+   console.log("data "+ids);
+}
+
 function send(){
 	console.log(ids);
 	console.log("aaa");
@@ -80,4 +73,3 @@ console.log("ERROR" +data);
     
   });
 }
-
--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -4,20 +4,25 @@ function check(i){
     console.log(i)
     if(document.getElementById('check_'+i).checked) {
     ids.push(""+i);
+
 	} 
 	else {
 	ids.pop(""+i);
     }
     if(ids.length>0){
-     $("#submit").prop('disabled', false);
+     $("#sendemailid").prop('disabled', false);
+     $("#tagbtnid").prop('disabled', false);
     }else{
-     $("#submit").prop('disabled', true);
+     $("#sendemailid").prop('disabled', true);
+     $("#tagbtnid").prop('disabled', true);
     }
    console.log(ids);
 }
 $(document).ready(function(){
-  $("#submit").prop('disabled', true);
- console.log("loaded" );
+  $("#sendemailid").prop('disabled', true);
+  $("#tagbtnid").prop('disabled', true);
+  
+ console.log("loaded tags" );
 
 });
 function send(){
